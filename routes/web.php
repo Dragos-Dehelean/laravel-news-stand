c6052fb68f314acc88d2c830fc48c26cfc51df2f
Dragos Dehelean <dragos.dehelean@selenis.ro>
2018-01-22 06:23:03 +0200

    small updates

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,6 @@ use Illuminate\Http\Request;
 
 Route::get('/', 'ArticleController@index')->name('home');
 
-
 Route::get('/auth/register', 'RegistrationController@register')->name('auth.register');
 
 Route::post('/auth/register', 'RegistrationController@postRegister')->name('auth.store');
@@ -36,6 +35,3 @@ Route::get('myindex', 'ArticleController@myindex')->name('myindex');
 Route::get('pdf/{article_id}', 'ArticleController@pdf');
 
 Route::get('feed/{type?}', ['as' => 'feed.atom', 'uses' => 'FeedController@getFeed']);
-
-
-
